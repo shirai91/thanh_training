493f48c98610d45be4229c240049fb32e02b6003
DESKTOP-RAAIMNI\ACER <exanp>
2021-06-20 15:53:05 +0700

    no message

--- 1_data_struct_and_algorithm/bai5.js
+++ 1_data_struct_and_algorithm/bai5.js
@@ -1,5 +1,7 @@
 var array = [1, 2, 3, 4, 5]
-var sum;
-for (var i = 0; i <= array.length; i++) {
-    console.log(sum += array.length)
-}
+var sum = 0;
+for (var i = 0; i < array.length; i++) {
+    sum += array[i++]
+};
+
+console.log("Tổng các số là " + sum);
