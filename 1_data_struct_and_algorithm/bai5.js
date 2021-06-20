ec333eaf0fc2506c740b6a70ae2fe7a22e99eccf
DESKTOP-RAAIMNI\ACER <exanp>
2021-06-20 14:50:31 +0700

    no message

--- 1_data_struct_and_algorithm/bai5.js
+++ 1_data_struct_and_algorithm/bai5.js
@@ -2,4 +2,4 @@ var array = [1, 2, 3, 4, 5]
 var sum;
 for (var i = 0; i <= array.length; i++) {
     console.log(sum += array.length)
-};
+}
---
+++ 1_data_struct_and_algorithm/bai6.js
@@ -0,0 +1,3 @@
+var arr = [1, 2, 3, 4];
+var total = 0;
+for (var i in arr) { total += arr[i]; }
