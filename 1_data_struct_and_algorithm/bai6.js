da060d9eb1f6fab89bfdc1158dda7c5db278413e
DESKTOP-RAAIMNI\ACER <exanp>
2021-07-05 09:19:08 +0700

    complete lab function convertTemperatureFToC(F) {

--- 1_data_struct_and_algorithm/bai6.js
+++ 1_data_struct_and_algorithm/bai6.js
@@ -3,5 +3,4 @@ var sum = 0;
 for (var i = 0; i < array.length; i++) {
     sum += array[i++]
 };
-
 console.log("Tổng các số là " + sum);
--- 1_data_struct_and_algorithm/print_array_members.js
+++ 1_data_struct_and_algorithm/print_array_members.js
@@ -1,5 +1,4 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
-
 array.forEach((item) => {
   console.log(item);
 });
